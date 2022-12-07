49d9eaf4ff4e16e8db2659b3c9a5c77eae2d3c7a
Nacho <silveraignacio>
2022-12-07 16:08:58 +0100

    Version 1.0

--- genpass.py
+++
@@ -1,6 +0,0 @@
-import getpass
-import bcrypt
-
-password = getpass.getpass("password: ")
-hashed_password = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
-print(hashed_password.decode())
